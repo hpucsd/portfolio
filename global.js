dd2874203fd6f7ee1e6082ab420ad0de716ed3b1
hpucsd <hputhenparambil@ucsd.edu>
2025-01-24 22:26:39 -0800

    Step 3.1

--- global.js
+++ global.js
@@ -13,3 +13,29 @@ let currentLink = navLinks.find(
 currentLink?.classList.add('current');
 
 console.log(navLinks);
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'contacts/', title: 'Contact' },
+  { url: 'projects/', title: 'Project' },
+  { url: 'https://github.com/hpucsd', title: 'Github' },
+  { url: 'resume/', title: 'Resume' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+// Create link and add it to nav
+nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+if (!ARE_WE_HOME && !url.startsWith('http')) {
+  url = '../' + url;
+}
