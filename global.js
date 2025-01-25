61c38be669f1e74e24a53a599513ca7482f55b10
hpucsd <hputhenparambil@ucsd.edu>
2025-01-24 23:08:39 -0800

    Step 3.2

--- global.js
+++ global.js
@@ -37,6 +37,13 @@ for (let p of pages) {
     url = '../' + url;
   }
 
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-  
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
 }
