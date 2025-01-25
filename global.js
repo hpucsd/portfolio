4063e4bebf79d7241ee5db0914ed6243948407ef
hpucsd <hputhenparambil@ucsd.edu>
2025-01-24 23:03:19 -0800

    Step 3.1

--- global.js
+++ global.js
@@ -25,14 +25,18 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-}
-
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
-if (!ARE_WE_HOME) {
-  url = '../' + url;
+
+for (let p of pages) {
+
+  let url = p.url;
+  let title = p.title;
+
+  if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = '../' + url;
+  }
+
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  
 }
