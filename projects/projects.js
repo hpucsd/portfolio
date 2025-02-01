bd6a0fb6dda6234b5fdefad6c9e6b409a2a4de92
hpucsd <hputhenparambil@ucsd.edu>
2025-02-01 14:12:31 -0800

    Steps 1.6

--- projects/projects.js
+++ projects/projects.js
@@ -2,6 +2,10 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 
+const titleElement = document.querySelector('.projects-title');
+
 const projectsContainer = document.querySelector('.projects');
+
+titleElement.textContent = `Projects (${projects.length})`;
 
 renderProjects(projects, projectsContainer, 'h2')
