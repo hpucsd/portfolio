079442f381830f0df7bce3de5f3991d00610ed50
hpucsd <hputhenparambil@ucsd.edu>
2025-01-31 19:04:49 -0800

    Steps 1.5

--- projects/projects.js
+++ projects/projects.js
@@ -2,4 +2,6 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 
+const projectsContainer = document.querySelector('.projects');
+
 renderProjects(projects, projectsContainer, 'h2')
