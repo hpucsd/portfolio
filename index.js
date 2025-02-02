de5619e5662807db516fdd0354b8456133137356
hpucsd <hputhenparambil@ucsd.edu>
2025-02-01 22:00:31 -0800

    Steps 3-5

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const latestProjects = projects.slice(0, 3);
 
 renderProjects(latestProjects, projectsContainer, 'h3');
 
-const githubData = await fetchGitHubData('giorgianicolaou');
+const githubData = await fetchGitHubData('hpuscd');
 
 const profileStats = document.querySelector('#profile-stats');
 
