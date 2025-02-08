16c942fe7bb3df4792b69d3ea37041520b57883f
hpucsd <hputhenparambil@ucsd.edu>
2025-02-07 22:47:53 -0800

    1.5

--- projects/projects.js
+++ projects/projects.js
@@ -52,6 +52,5 @@ arcs.forEach((arc, idx) => {
     d3.select('svg')
       .append('path')
       .attr('d', arc)
-      .attr(colors(idx)) // Fill in the attribute for fill color via indexing the colors variable
-})
-
+      .attr("fill", colors(idx)); // Fill in the attribute for fill color via indexing the colors variable
+});
