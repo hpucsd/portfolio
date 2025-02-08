4ae3cf6e97320b7172e62ae3d1d3d4d9e18cf257
hpucsd <hputhenparambil@ucsd.edu>
2025-02-07 22:52:05 -0800

    1.5

--- projects/projects.js
+++ projects/projects.js
@@ -25,7 +25,14 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 // d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+];
 
 let total = 0;
 
@@ -35,7 +42,7 @@ for (let d of data) {
 
 let angle = 0;
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 
 for (let d of data) {
@@ -54,3 +61,10 @@ arcs.forEach((arc, idx) => {
       .attr('d', arc)
       .attr("fill", colors(idx)); // Fill in the attribute for fill color via indexing the colors variable
 });
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+})
