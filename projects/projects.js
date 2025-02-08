17278b5b11be4943b1948482d40bd7069e35d80b
hpucsd <hputhenparambil@ucsd.edu>
2025-02-07 22:37:30 -0800

    1.5

--- projects/projects.js
+++ projects/projects.js
@@ -16,7 +16,7 @@ renderProjects(projects, projectsContainer, 'h2')
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
-// let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 // let arc = arcGenerator({
 //     startAngle: 0,
@@ -25,7 +25,7 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 // d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-let data = [1, 2];
+let data = [1, 2, 3, 4, 5, 5];
 
 let total = 0;
 
@@ -34,7 +34,9 @@ for (let d of data) {
 }
 
 let angle = 0;
-let arcData = [];
+
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 
 for (let d of data) {
   let endAngle = angle + (d / total) * 2 * Math.PI;
@@ -44,12 +46,12 @@ for (let d of data) {
 
 let arcs = arcData.map((d) => arcGenerator(d));
 
-let colors = ['gold', 'purple'];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 arcs.forEach((arc, idx) => {
     d3.select('svg')
       .append('path')
       .attr('d', arc)
-      .attr(colors) // Fill in the attribute for fill color via indexing the colors variable
+      .attr(colors(1), colors(3), colors(4), colors(7), colors(9)) // Fill in the attribute for fill color via indexing the colors variable
 })
 
