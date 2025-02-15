3d41854e0f20ca970849e8d196281910cebd87bf
hpucsd <hputhenparambil@ucsd.edu>
2025-02-14 21:09:14 -0800

    Step 1.2

--- meta/main.js
+++ meta/main.js
@@ -10,8 +10,40 @@ async function loadData() {
         datetime: new Date(row.datetime),
       }));
   console.log(data);
+  processCommits();
+  console.log(commits);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
 });
+
+let commits = d3.groups(data, (d) => d.commit);
+
+function processCommits() {
+  commits = d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        // What other options do we need to set?
+        // Hint: look up configurable, writable, and enumerable
+      });
+
+      return ret;
+    });
+}
