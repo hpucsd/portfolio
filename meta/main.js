b38427ce030915bce10ad242e28808b02d4e2d9f
hpucsd <hputhenparambil@ucsd.edu>
2025-02-14 21:41:22 -0800

    Step 2.1 and 2.2

--- meta/main.js
+++ meta/main.js
@@ -1,6 +1,7 @@
 console.log("JavaScript file loaded!");
 
 let data = [];
+let commits = [];
 
 async function loadData() {
     data = await d3.csv('loc.csv', (row) => ({
@@ -14,13 +15,12 @@ async function loadData() {
   console.log(data);
   processCommits();
   console.log(commits);
+  displayStats()
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
 });
-
-let commits = d3.groups(data, (d) => d.commit);
 
 function processCommits() {
   commits = d3
@@ -44,6 +44,9 @@ function processCommits() {
         value: lines,
         // What other options do we need to set?
         // Hint: look up configurable, writable, and enumerable
+        enumerable: false,
+        configurable: false,
+        writable: false,
       });
 
       return ret;
@@ -70,4 +73,65 @@ function displayStats() {
   const numFiles = d3.group(data, (d) => d.file).size;
   dl.append('dt').text('Number of files');
   dl.append('dd').text(numFiles);
+}
+
+function createScatterplot() {
+    const width = 1000;
+    const height = 600;
+
+    const svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('viewBox', `0 0 ${width} ${height}`)
+      .style('overflow', 'visible');
+
+    const xScale = d3
+      .scaleTime()
+      .domain(d3.extent(commits, (d) => d.datetime))
+      .range([0, width])
+      .nice();
+
+    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+    const dots = svg.append('g').attr('class', 'dots');
+
+    dots
+      .selectAll('circle')
+      .data(commits)
+      .join('circle')
+      .attr('cx', (d) => xScale(d.datetime))
+      .attr('cy', (d) => yScale(d.hourFrac))
+      .attr('r', 5)
+      .attr('fill', 'steelblue');
+
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+    
+    const usableArea = {
+      top: margin.top,
+      right: width - margin.right,
+      bottom: height - margin.bottom,
+      left: margin.left,
+      width: width - margin.left - margin.right,
+      height: height - margin.top - margin.bottom,
+    };
+    
+    // Update scales with new ranges
+    xScale.range([usableArea.left, usableArea.right]);
+    yScale.range([usableArea.bottom, usableArea.top]);
+
+    // Create the axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale).tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+    // Add X axis
+    svg
+      .append('g')
+      .attr('transform', `translate(0, ${usableArea.bottom})`)
+      .call(xAxis);
+
+    // Add Y axis
+    svg
+      .append('g')
+      .attr('transform', `translate(${usableArea.left}, 0)`)
+      .call(yAxis);
 }
