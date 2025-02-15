93fe075eedc5a8c6b9b50c706ef9671b8d125c42
hpucsd <hputhenparambil@ucsd.edu>
2025-02-14 23:57:16 -0800

    Step 5

--- meta/main.js
+++ meta/main.js
@@ -2,6 +2,8 @@ console.log("JavaScript file loaded!");
 
 let data = [];
 let commits = [];
+let xScale
+let yScale
 
 async function loadData() {
   data = await d3.csv('loc.csv', (row) => ({
@@ -58,14 +60,6 @@ function processCommits() {
   commits = d3.sort(commits, (d) => -d.totalLines);
   console.log("Sorted commits:", commits);
 }
-
-let xScale = d3
-      .scaleTime()
-      .domain(d3.extent(sortedCommits, (d) => d.datetime))
-      .range([0, width])
-      .nice();
-
-let yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
 
 function displayStats() {
   const dl = d3.select('#stats').append('dl').attr('class', 'stats');
